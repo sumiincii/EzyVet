8b2b21c7d11faaebeeb61f8cf99bdd1b2f723ffd
SUMI <santosvincentj@gmail.com>
2024-04-06 23:28:24 +0800

    header palang to

---
+++ btstrp.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="_assets/bootstrap.min.css" />
+</head>
+<style>
+    [class*="col"]{
+        padding: 1rem;;
+        background-color: #33b5e5;
+        border: 2px solid #fff;
+        color: #fff;
+    }
+</style>
+<body>
+    <div class="container-md border"> 
+        <div class="row">
+            <div class="col-4">Col 1</div>
+            <div class="col-4">Col 2</div>
+            <div class="col-4">Col 3</div>
+        </div>
+    </div>
+
+
+
+
+
+
+<script src="_assets/bootstrap.min.js"></script>
+<script src="_assets/jquery.min.js"></script>
+</body>
+</html>
