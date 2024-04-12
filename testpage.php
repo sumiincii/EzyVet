14adecc75820403b241db5bc9bade8d483acf2e7
SUMI <santosvincentj@gmail.com>
2024-04-13 00:19:23 +0800

    asd

--- testpage.php
+++ testpage.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <link rel="stylesheet" href="css/testpage.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ezyvet</title>
@@ -8,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/testpage.css">
+    
     <link rel="stylesheet" type="text/css" href="_assets/bootstrap.min.css" />
 </head>
 <body>
@@ -57,12 +58,14 @@
 <br>
 <br>
 <br>
+<br>
+<br>
 
 
 <div class="container2 container-fluid text-center">
   <div class="row">
     <div class="sentence col-6">
-      <p>YOUR PET'S SANCTUARY <br>FOR COMPASSIONATE<br><p style="font-family: Rock Salt !important;font-size: 40px;line-height:49px;"><b>VETERINARY CARE</b></p></p>
+      <p>YOUR PET'S SANCTUARY <br>FOR COMPASSIONATE<br><p style="font-family: Rock Salt !important;font-size: 39px;line-height:49px;"><b>VETERINARY CARE</b></p></p>
     </div>
     <div class="col-6">
       <h1>image here</h1>
@@ -75,19 +78,26 @@
 <br>
 <br>
 
+
+
+<div class="pangalawa">
 <div class="container text-center">
   <div class="row">
     <div class="col align-self-center">
       One of three columns
     </div>
     <div class="col-7 align-self-center">
-      <p style=" padding: 50px;font-family: Montserrat !important;letter-spacing: normal;font-size: 15px;text-align: center;line-height: 22px; color: #3e444b;">Being a pet owner, you are aware of the importance of regular veterinary checkups for your animal's health. But sometimes you might wonder whether the stress of visiting the veterinarian is really worth your pet's fear and anxiety. You don't have to choose between your pet's mental health and veterinary care at Dr. Ron Veterinary Clinic because we are committed to providing high-quality, holistic veterinarian care with an intense focus on a fear-free approach.</p>
+      <p>Discover expert care and compassionate service</p>
+      <hr style="margin-top:0; margin-bottom:0;">
+      <p style=" padding: 50px;font-family: Montserrat !important;letter-spacing: normal;font-size: 15px;text-align: center;line-height: 22px; color: #3e444b;">Being a pet owner, you are aware of the importance of regular veterinary checkups for your animal's health. But sometimes you might wonder whether the stress of visiting the veterinarian is really worth your pet's fear and anxiety. You don't have to choose between your pet's mental health and veterinary care at Dr. Ron Veterinary Clinic because we are committed to providing high-quality, holistic veterinarian care with an intense focus on a fear-free approach.</p>
     </div>
     <div class="col align-self-center">
       One of three columns
     </div>
   </div>
 </div>
+</div>
+
 
 <script src="_assets/bootstrap.min.js"></script>
 <script src="_assets/jquery.min.js"></script>
