99c8f5929f3996a7a3ec3fdcaf393967a83d6b7d
SUMI <santosvincentj@gmail.com>
2024-04-13 17:49:06 +0800

    with footer

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="testpage.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ezyvet</title>
@@ -8,155 +9,136 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" type="text/css" href="_assets/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="_assets/bootstrap.min.css"/>
 </head>
-<style>
-    body{
-        background-color: #ffffff;
-        font-family: 'Montserrat'  , sans-serif !important;
-    }
-    [class*="col"]{
-        padding: 0;
-        background-color: #ffffff;
-        border: 2px solid #fff;
-        
-    }
-    
-    [class*="container"]{
-        /* color: #fff; */
-    }
-    #wc{
-        background-color: #c1cad3;
-        padding: 7px;
-        color: #3e444b;
-    }
-    #logo{
-        text-align: start;
-        padding: 14px;
-        color: darkslategray;
-    }
-    nav a::before{
-        content: '';
-        position: absolute;
-        top:100%;
-        left: 0;
-        width: 0;
-        height: 2.5px;
-        background: #c1cad3;
-        transition: .3s;
-    }
-    nav a:hover::before {
-        width: 100%;
-
-    }
-    nav{
-        margin-top: 10px;
-        background: #fff;
-        
-}
-.logo{
-    padding: 5px 0 0 90px;
-    width: 150px;
-    height: auto;
-}
-nav ul{
-    padding: 0;
-    margin: 0;
-    float: right;
-    margin-right: 102px;
-    
-}
-nav ul li{
-    background: #fff;
-    position: relative;
-    list-style: none;
-    display: inline-block;  
-}
-nav ul li a{
-    
-    display: block;
-    padding: 0 25px;
-    color: #3e444b;
-    text-decoration: none;
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 60px;
-    letter-spacing: 2px;
-    
-    
-}
-
-.hober a:hover{
-    background: rgb(150, 150, 150);
-}
-nav ul ul{
-    position: absolute;
-    top: 65px;
-    display: none;
-    
-}
-nav ul li:hover > ul{
-    display: block;
-    box-shadow: 0 7px 14px rgba(0, 0, 0, 1);
-    border: 1px solid #3e444b; 
-    
-}
-nav ul ul li{
-    width: 159px;
-    float: none;
-    display: list-item;
-    position: relative;
-}
-nav ul ul ul li{
-    position: relative;
-}
-
-</style>
 <body>
-    <div class="container-fluid text-center">
+    <div class="container1 container-fluid text-center">
         <div class="row">
             <div class="col" id="wc">Welcome to <b>Dr. Ron Veterinary Clinic</b> , your trusted partner in providing top-notch veterinary care for your beloved pets.</div>
         </div>
     </div>
-
-    <div class="container-fluid text-center"> 
+<img class="logo img-fluid float-start" src="images/ezyvet.png" alt="logo">
+    <div class="container-fluid text-center">
         <div class="dropdown">
-        <nav>
-            <img class="logo" src="/images/cafeterialogo.png">
-            <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">ABOUT</a>
-                    <ul>
-                        <li class="hober"><a href="#">asd</a></li>
-                        <li class="hober"><a href="#">asd</a></li>
-                        
-                    </ul>
-                </li>
-                <li><a href="#">SERVICES</a>
-                    <ul>
-                        <li class="hober"><a href="#">asd</a></li>
-                        
-                        <li class="hober"><a href="#">asd</a>
-                            
-                        </li>
-                    </ul>
-                </li>
-                <li><a href="contact_form.html">CONTACT US</a></li>
-                <!-- <li><a href="feedback.html">FEEDBACK</a></li>
-                <li><a href="loginpage.html">LOG OUT</a></li> -->
-            </ul>
-        </nav>
+            <nav>
+                    
+                <ul>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">SERVICES</a>
+                        <ul>
+                            <li class="hober"><a href="#">asd</a></li>
+                            <li class="hober"><a href="#">asd</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">BOOK NOW</a>
+                        <ul>
+                            <li class="hober"><a href="#">asd</a></li>
+                            <li class="hober"><a href="#">asd</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="#">CONTACT US</a></li>
+                </ul>
+            </nav>
+        </div>
     </div>
     </div>
+<br>
+<br>
 
-    <div class="container-fluid">
 
-    
+
+
+<!-- <p>YOUR PET'S SANCTUARY <br>FOR COMPASSIONATE<br><p style="font-family: Rock Salt !important;font-size: 39px;line-height:49px;"><b>VETERINARY CARE</b></p></p> -->
+
+<!-- <img src="images/asologo.png" alt="asologo" class="asologo"> -->
+
+
+<!-- <div class="containeruna container-fluid ">
+<div class="row ">
+  <div class="col"></div>
+  <div class="col-6">
+    <img src="images/asologo.png" alt="asologo" class="asologo">
+  </div>
+</div>
+<div class="row ">
+  <div class="col"></div>
+  <div class="col"></div>
+</div>
+
+</div> -->
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div class="containeruna ">
+  <div class="row">
+    <div class="col-6 sentence">
+      <p id="vetcare">YOUR PET'S SANCTUARY <br>FOR COMPASSIONATE<br><p style="font-family: Rock Salt !important;font-size: 39px;line-height:49px;"><b>VETERINARY CARE</b></p></p>
+      <button type="button" class="btn btn-outline-success button1">REQUEST AN APPOINTMENT</button>
+    </div>
+    <!-- <div class="col-1">
+      
+    </div> -->
+    <div class="col-6 align-self-right">
+      <img src="images/asologo.png" alt="asologo" class="asologo float-start">
+    </div>
+  </div>
+  
+</div>
+
+<!-- <div class="containeruna container-fluid text-center">
+  <div class="row ">
+    <div class="col align-self-start">
+      
     </div>
 
-    
+    <div class="col">
+    </div>
+  </div>
+</div> -->
 
+
+
+
+<!-- <div class="container2 container-fluid text-center">
+  <div class="row">
+    <div class="sentence col-6 align-self-left">
+      <p>YOUR PET'S SANCTUARY <br>FOR COMPASSIONATE<br><p style="font-family: Rock Salt !important;font-size: 39px;line-height:49px;"><b>VETERINARY CARE</b></p></p>
+    </div>
+    <div class="col-6">
+      </div>
+    </div>
+  </div> -->
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div class="pangalawa">
+<div class="container3 text-center">
+  <div class="row">
+    <div class="col align-self-center">
+      <!-- wala -->
+    </div>
+    <div class="col-8 align-self-center gitna">
+      <img src="images/Paws.png" class="rounded mx-auto d-block paws-logo" alt="PawsLogo">
+      <p id="p1">DR. RON VETERINARY ANIMAL CLUB</p>
+      <p id="p2">Discover Expert Care And Compassionate Service</p>
+      <hr style="margin-top:0; margin-bottom:0;">
+      <p id="p3">Being a pet owner, you are aware of the importance of regular veterinary checkups for your animal's health. But sometimes you might wonder whether the stress of visiting the veterinarian is really worth your pet's fear and anxiety. You don't have to choose between your pet's mental health and veterinary care at Dr. Ron Veterinary Clinic because we are committed to providing high-quality, holistic veterinarian care with an intense focus on a fear-free approach.</p>
+    </div>
+    <div class="col align-self-center">
+     <!-- wala pa -->
+    </div>
+  </div>
+</div>
+</div>
 
 
 <script src="_assets/bootstrap.min.js"></script>
--- testpage.php
+++ testpage.php
@@ -1,60 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <link rel="stylesheet" href="testpage.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ezyvet</title>
-    <!-- this is my fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="_assets/bootstrap.min.css"/>
+  <link rel="stylesheet" href="css/testpage.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ezyvet</title>
+  <!-- this is my fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="_assets/bootstrap.min.css" />
 </head>
+
 <body>
-    <div class="container1 container-fluid text-center">
-        <div class="row">
-            <div class="col" id="wc">Welcome to <b>Dr. Ron Veterinary Clinic</b> , your trusted partner in providing top-notch veterinary care for your beloved pets.</div>
-        </div>
+  <div class="container1 container-fluid text-center">
+    <div class="row">
+      <div class="col" id="wc">Welcome to <b>Dr. Ron Veterinary Clinic</b> , your trusted partner in providing top-notch veterinary care for your beloved pets.</div>
     </div>
-<img class="logo img-fluid float-start" src="images/ezyvet.png" alt="logo">
-    <div class="container-fluid text-center">
-        <div class="dropdown">
-            <nav>
-                    
-                <ul>
-                    <li><a href="#">ABOUT</a></li>
-                    <li><a href="#">SERVICES</a>
-                        <ul>
-                            <li class="hober"><a href="#">asd</a></li>
-                            <li class="hober"><a href="#">asd</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">BOOK NOW</a>
-                        <ul>
-                            <li class="hober"><a href="#">asd</a></li>
-                            <li class="hober"><a href="#">asd</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li><a href="#">CONTACT US</a></li>
-                </ul>
-            </nav>
-        </div>
+  </div>
+  <img class="logo img-fluid float-start" src="images/ezyvet.png" alt="logo">
+  <div class="container-fluid text-center">
+    <div class="dropdown">
+      <nav>
+        <ul>
+          <li><a href="#">ABOUT</a></li>
+          <li><a href="#">SERVICES</a>
+            <ul>
+              <li class="hober"><a href="#">asd</a></li>
+              <li class="hober"><a href="#">asd</a></li>
+            </ul>
+          </li>
+          <li><a href="#">BOOK NOW</a>
+            <ul>
+              <li class="hober"><a href="#">asd</a></li>
+              <li class="hober"><a href="#">asd</a>
+              </li>
+            </ul>
+          </li>
+          <li><a href="#">CONTACT US</a></li>
+        </ul>
+      </nav>
     </div>
-    </div>
-<br>
-<br>
+  </div>
+  </div>
+  <br>
+  <br>
 
 
 
 
-<!-- <p>YOUR PET'S SANCTUARY <br>FOR COMPASSIONATE<br><p style="font-family: Rock Salt !important;font-size: 39px;line-height:49px;"><b>VETERINARY CARE</b></p></p> -->
+  <!-- <p>YOUR PET'S SANCTUARY <br>FOR COMPASSIONATE<br><p style="font-family: Rock Salt !important;font-size: 39px;line-height:49px;"><b>VETERINARY CARE</b></p></p> -->
 
-<!-- <img src="images/asologo.png" alt="asologo" class="asologo"> -->
+  <!-- <img src="images/asologo.png" alt="asologo" class="asologo"> -->
 
 
-<!-- <div class="containeruna container-fluid ">
+  <!-- <div class="containeruna container-fluid ">
 <div class="row ">
   <div class="col"></div>
   <div class="col-6">
@@ -68,30 +69,32 @@
 
 </div> -->
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
 
-<div class="containeruna ">
-  <div class="row">
-    <div class="col-6 sentence">
-      <p id="vetcare">YOUR PET'S SANCTUARY <br>FOR COMPASSIONATE<br><p style="font-family: Rock Salt !important;font-size: 39px;line-height:49px;"><b>VETERINARY CARE</b></p></p>
-      <button type="button" class="btn btn-outline-success button1">REQUEST AN APPOINTMENT</button>
-    </div>
-    <!-- <div class="col-1">
+  <div class="containeruna ">
+    <div class="row">
+      <div class="col-6 sentence">
+        <p id="vetcare">YOUR PET'S SANCTUARY <br>FOR COMPASSIONATE<br>
+        <p style="font-family:Allura !important;font-size: 80px;line-height:49px; font-weight:350;"><b>Veterinary Care</b></p>
+        </p>
+        <button type="button" class="btn btn-success button1">REQUEST AN APPOINTMENT</button>
+      </div>
+      <!-- <div class="col-1">
       
     </div> -->
-    <div class="col-6 align-self-right">
-      <img src="images/asologo.png" alt="asologo" class="asologo float-start">
+      <div class="col-6 align-self-right">
+        <img src="images/asologo.png" alt="asologo" class="asologo float-start">
+      </div>
     </div>
-  </div>
-  
-</div>
 
-<!-- <div class="containeruna container-fluid text-center">
+  </div>
+
+  <!-- <div class="containeruna container-fluid text-center">
   <div class="row ">
     <div class="col align-self-start">
       
@@ -105,7 +108,7 @@
 
 
 
-<!-- <div class="container2 container-fluid text-center">
+  <!-- <div class="container2 container-fluid text-center">
   <div class="row">
     <div class="sentence col-6 align-self-left">
       <p>YOUR PET'S SANCTUARY <br>FOR COMPASSIONATE<br><p style="font-family: Rock Salt !important;font-size: 39px;line-height:49px;"><b>VETERINARY CARE</b></p></p>
@@ -114,34 +117,76 @@
       </div>
     </div>
   </div> -->
-<br>
-<br>
-<br>
-<br>
-<br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
 
-<div class="pangalawa">
-<div class="container3 text-center">
-  <div class="row">
-    <div class="col align-self-center">
-      <!-- wala -->
-    </div>
-    <div class="col-8 align-self-center gitna">
-      <img src="images/Paws.png" class="rounded mx-auto d-block paws-logo" alt="PawsLogo">
-      <p id="p1">DR. RON VETERINARY ANIMAL CLUB</p>
-      <p id="p2">Discover Expert Care And Compassionate Service</p>
-      <hr style="margin-top:0; margin-bottom:0;">
-      <p id="p3">Being a pet owner, you are aware of the importance of regular veterinary checkups for your animal's health. But sometimes you might wonder whether the stress of visiting the veterinarian is really worth your pet's fear and anxiety. You don't have to choose between your pet's mental health and veterinary care at Dr. Ron Veterinary Clinic because we are committed to providing high-quality, holistic veterinarian care with an intense focus on a fear-free approach.</p>
-    </div>
-    <div class="col align-self-center">
-     <!-- wala pa -->
+  <div>
+    <div class="container3 text-center">
+      <div class="row row-number2">
+        <div class="col align-self-center wala">
+          <!-- wala -->
+        </div>
+        <div class="col-8 align-self-center gitna">
+          <img src="images/Paws.png" class="rounded mx-auto d-block paws-logo" alt="PawsLogo">
+          <p id="p1">DR. RON VETERINARY ANIMAL CLUB</p>
+          <p id="p2">Discover Expert Care And Compassionate Service</p>
+          <hr style="margin-top:5px; margin-bottom:5px;">
+          <p id="p3">Being a pet owner, you are aware of the importance of regular veterinary checkups for your animal's health. But sometimes you might wonder whether the stress of visiting the veterinarian is really worth your pet's fear and anxiety. You don't have to choose between your pet's mental health and veterinary care at Dr. Ron Veterinary Clinic because we are committed to providing high-quality, holistic veterinarian care with an intense focus on a fear-free approach.</p>
+        </div>
+        <div class="col align-self-center wala">
+          <!-- wala pa -->
+        </div>
+      </div>
     </div>
   </div>
-</div>
-</div>
 
 
-<script src="_assets/bootstrap.min.js"></script>
-<script src="_assets/jquery.min.js"></script>
+
+  <div class="container4 container-fluid text-center">
+    <div class="border">
+      <img src="images/Paws.png" class="rounded mx-auto d-block paws-logo" alt="PawsLogo">
+      <p>Visit Us.</p>
+      <button type="button" class="btn btn-success button2">REQUEST AN APPOINTMENT</button>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+  <script src="_assets/bootstrap.min.js"></script>
+  <script src="_assets/jquery.min.js"></script>
 </body>
+
 </html>
+<footer class="container-fluid text-center footer">
+  <div class="row">
+    <div class="col-4">
+      <!-- Embed a map here -->
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.338564653656!2d-1.8208356846043523!3d53.48321568002524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487bdf2e46e9e0b1%3A0x8469e8a6832f5c6e!2sDr.%20Ron%20Veterinary%20Clinic!5e0!3m2!1sen!2suk!4v1646022342255!5m2!1sen!2suk" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+    <div class="col-4">
+      <img src="images/ezyvet.png" alt="logo" class="logo-footer">
+      <p>Dr. Ron Veterinary Clinic</p>
+      <p>Location: [Your Address]</p>
+      <p>Opening Hours: [Your Opening Hours]</p>
+      <p>Closing Hours: [Your Closing Hours]</p>
+    </div>
+    <div class="col-4">
+      <!-- You could add another section here, like contact information -->
+      <p>Contact Us:</p>
+      <p>Phone: [Your Phone Number]</p>
+      <p>Email: [Your Email]</p>
+    </div>
+  </div>
+</footer>
