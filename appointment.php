ce9ae4651dc05109d565820346b68a488959a85c
SUMI <santosvincentj@gmail.com>
2024-07-24 18:16:54 +0800

    Update appointment.php

--- appointment.php
+++ appointment.php
@@ -17,7 +17,7 @@
 <body>
     <div class="container1 container-fluid text-center">
         <div class="row">
-            <div class="col" id="wc">Welcome to <b>Dr. Ron Veterinary Clinic</b> , your trusted partner in providing top-notch veterinary care for your beloved pets.</div>
+            <div class="col" id="wc">Welcome to <b>Dr. Ron Veterinary Clinic</b>, your trusted partner in providing top-notch veterinary care for your beloved pets.</div>
         </div>
     </div>
     <a href="testpage.php"><img class="logo img-fluid float-start" src="images/ezyvetnewlogo.png" alt="logo"></a>
@@ -35,22 +35,12 @@
                             <li class="hober"><a href="followup.php">Follow Up</a></li>
                         </ul>
                     </li>
-                    <li><a href="appointment.php">BOOK NOW</a>
-                        <!-- <ul>
-              <li class="hober1"><a href="appointment.php">REQUESTANAPPOINTMENT </a></li>
-              <li class="hober"><a href="#">asd</a>
-              </li>
-            </ul> -->
-                    </li>
+                    <li><a href="appointment.php">BOOK NOW</a></li>
                     <li><a href="contactus.php">CONTACT US</a></li>
                 </ul>
             </nav>
         </div>
     </div>
-    </div>
-    <br>
-    <br>
-
     <br>
     <br>
     <br>
@@ -61,7 +51,7 @@
     <br>
     <br>
 
-    <div class=" container1 container-fluid p-0" style="overflow:visible;">
+    <div class="container1 container-fluid p-0" style="overflow:visible;">
         <div class="row g-0">
             <div class="col-12 image-top">
                 <img src="images/booknow1.jpg" alt="dog image" class="img-fluid w-100 h-100" />
@@ -70,7 +60,7 @@
     </div>
 
     <div class="container my-5">
-        <h2 class="text-center ">Make your Appointment Request Online</h2>
+        <h2 class="text-center">Make your Appointment Request Online</h2>
         <div class="row">
             <p>Planning a visit? Fill out our appointment request form below and hit 'submit.' Please schedule your appointment at least 24 hours in advance so we can confirm it with you. In case of an emergency, don't use this form—call us at (860) 757-3346 or visit our clinic.</p>
         </div>
@@ -81,27 +71,27 @@
                     <div class="mb-3">
                         <label class="form-label">Are You A New Client *</label>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="newClient" id="newClientYes" value="yes" />
+                            <input class="form-check-input" type="radio" name="newClient" id="newClientYes" value="yes" required />
                             <label class="form-check-label" for="newClientYes">Yes</label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="newClient" id="newClientNo" value="no" />
+                            <input class="form-check-input" type="radio" name="newClient" id="newClientNo" value="no" required />
                             <label class="form-check-label" for="newClientNo">No</label>
                         </div>
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Select Preferred Veterinarian *</label>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="preferredVet" id="sumi" value="monica" />
-                            <label class="form-check-label" for="sumi">Dr. sumi</label>
+                            <input class="form-check-input" type="radio" name="preferredVet" id="sumi" value="sumi" required />
+                            <label class="form-check-label" for="sumi">Dr. Sumi</label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="preferredVet" id="mano" value="john" />
-                            <label class="form-check-label" for="mano">Dr. mano</label>
+                            <input class="form-check-input" type="radio" name="preferredVet" id="mano" value="mano" required />
+                            <label class="form-check-label" for="mano">Dr. Mano</label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="preferredVet" id="vince" value="sarah" />
-                            <label class="form-check-label" for="vince">Dr. vince</label>
+                            <input class="form-check-input" type="radio" name="preferredVet" id="vince" value="vince" required />
+                            <label class="form-check-label" for="vince">Dr. Vince</label>
                         </div>
                     </div>
                     <div class="row">
@@ -114,10 +104,9 @@
                             <input type="text" class="form-control" id="lastName" required />
                         </div>
                     </div>
-
                     <div class="row">
                         <div class="col-sm-6 mb-3">
-                            <label for="email" class="form-label"> Email *</label>
+                            <label for="email" class="form-label">Email *</label>
                             <input type="email" class="form-control" id="email" required />
                         </div>
                         <div class="col-sm-6 mb-3">
@@ -125,14 +114,13 @@
                             <input type="number" class="form-control" id="phone" required />
                         </div>
                     </div>
-
                     <div class="mb-3">
                         <label for="appointmentDate" class="form-label">Preferred Day for Appointment *</label>
                         <input type="date" class="form-control" id="appointmentDate" required />
                     </div>
                     <div class="mb-3">
                         <label for="appointmentTime" class="form-label">Preferred Time for Appointment *</label>
-                        <input type="time" class="form-control" id="appointmentTime" required />
+                        <input type="time" class="form-control" id="appointmentTime" min="08:00" max="17:00" required />
                     </div>
                     <button type="submit" class="btn btn-success button1">SUBMIT</button>
                 </form>
