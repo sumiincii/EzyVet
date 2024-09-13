db3144ff064263cfbc773ee229bf3e27c125f83e
SUMI <santosvincentj@gmail.com>
2024-09-14 01:28:37 +0800

    "Refactored index.php to simplify HTML structure and removed unnecessary code, and updated send.php to modify PHPMailer settings and error handling."

--- index.php
+++ index.php
@@ -2,195 +2,20 @@
 <html lang="en">
 
 <head>
-  <link rel="stylesheet" href="css/index.css">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ezyvet</title>
-  <!-- this is my fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="_assets/bootstrap.min.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>send email</title>
 </head>
 
 <body>
-  <div class="container1 container-fluid text-center">
-    <div class="row">
-      <div class="col" id="wc">Welcome to <b>Dr. Ron Veterinary Clinic</b> , your trusted partner in providing top-notch veterinary care for your beloved pets.</div>
-    </div>
-  </div>
-  <a href="testpage.php"><img class="logo img-fluid float-start" src="images/ezyvet.png" alt="logo"></a>
+    <form action="send.php" method="post">
 
-  <div class="container-fluid text-center">
-    <div class="dropdown">
-      <nav>
-        <ul>
-          <li><a href="#">ABOUT</a></li>
-          <li><a href="">SERVICES</a>
-            <ul>
-              <li class="hober"><a href="#">asdasdasdasdasdasdasdasdasdasdasd</a></li>
-              <li class="hober"><a href="#">asasdasdasdasdasdasdasdd</a></li>
-            </ul>
-          </li>
-          <li><a href="appointment.php">BOOK NOW</a>
-            <!-- <ul>
-              <li class="hober1"><a href="appointment.php">REQUESTANAPPOINTMENT </a></li>
-              <li class="hober"><a href="#">asd</a>
-              </li>
-            </ul> -->
-          </li>
-          <li><a href="#">CONTACT US</a></li>
-        </ul>
-      </nav>
-    </div>
-  </div>
-  </div>
-  <br>
-  <br>
+        email<input type="email" name="email" value=""><br>
+        subject<input type="text" name="subject" value=""><br>
+        message<input type="text" name="message" value=""><br>
+        <button type="submit" name="send">send</button>
 
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-
-  <div class="containeruna ">
-    <div class="row">
-      <div class="col-6 sentence">
-        <p id="vetcare">YOUR PET'S SANCTUARY <br>FOR COMPASSIONATE<br>
-        <p style="font-family:Allura !important;font-size: 80px;line-height:49px; font-weight:350;"><b>Veterinary Care</b></p>
-        </p>
-        <button type="button" class="btn btn-success button1">REQUEST AN APPOINTMENT</button>
-      </div>
-      <div class="col-6 align-self-right">
-        <img src="images/asologo.png" alt="asologo" class="asologo float-start">
-      </div>
-    </div>
-
-  </div>
-
-  <!-- eto last ng header -->
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-
-  <div class="animate__animated animate__fadeInLeft">
-    <div class="container3 text-center">
-      <div class="row row-number2">
-        <div class="col align-self-center wala">
-          <!-- wala -->
-        </div>
-        <div class="col-8 align-self-center gitna">
-          <img src="images/Paws.png" class="rounded mx-auto d-block paws-logo" alt="PawsLogo">
-          <p id="p1">DR. RON VETERINARY ANIMAL CLUB</p>
-          <p id="p2">Discover Expert Care And Compassionate Service</p>
-          <hr style="margin-top:5px; margin-bottom:5px;">
-          <p id="p3">Being a pet owner, you are aware of the importance of regular veterinary checkups for your animal's health. But sometimes you might wonder whether the stress of visiting the veterinarian is really worth your pet's fear and anxiety. You don't have to choose between your pet's mental health and veterinary care at Dr. Ron Veterinary Clinic because we are committed to providing high-quality, holistic veterinarian care with an intense focus on a fear-free approach.</p>
-        </div>
-        <div class="col align-self-center wala">
-          <!-- wala pa -->
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-
-  <div class="container4 container-fluid text-center">
-    <div class="border">
-      <img src="images/Paws.png" class="rounded mx-auto d-block paws-logo" alt="PawsLogo">
-      <p>Visit Us.</p>
-      <button type="button" class="btn btn-success button2">REQUEST AN APPOINTMENT</button>
-    </div>
-  </div>
-
-  <script src="_assets/bootstrap.min.js"></script>
-  <script src="_assets/jquery.min.js"></script>
+    </form>
 </body>
-
-<footer class="containerfooter container-fluid text-center footer">
-  <div class="row">
-    <div class="col-7">
-      <!-- Embed a map here -->
-      <iframe src="https://www.google.com/maps/embed?pb=!3m2!1sen!2sph!4v1713428538080!5m2!1sen!2sph!6m8!1m7!1s1UPyzrm-fB9QmunjaXDomg!2m2!1d15.49962680830744!2d120.9768442687773!3f244.72785217855838!4f1.4193708654089647!5f0.7820865974627469" width="740" height="520" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
-
-    <div class="col-5 footerright">
-      <!-- <ul class="list-unstyled">
-        <li>Monday: 9:00 am - 6:00 pm</li>
-        <li>Tuesday: 9:00 am - 6:00 pm</li>
-      </ul> -->
-
-
-      <!-- <div class="col">
-        <img src="icons/geo.svg" alt="location">
-      </div>
-      <div class="col">
-        <display>
-          <h1>EZYVET</h1>
-        </display>
-        <p>Dr. Ron Veterinary Clinic</p>
-
-        <p>Mulawin St. Barangay Bitas, Cabanatuan City, Nueva Ecija</p>
-        <p>Opening Hours: 8:00AM</p>
-        <p>Closing Hours: 6:00PM</p>
-
-        <p>Phone: 0955-617-9958</p>
-        <p>Email: dronclinic@gmail.com</p>
-      </div> -->
-
-      <div class="row">
-        <h1>EZYVET</h1>
-      </div>
-
-      <div class="row">
-        <p>Dr. Ron Veterinary Clinic</p>
-      </div>
-
-      <!-- <div class="row iconsto1">
-        <div class="col-5 lg"><img src="icons/geo.svg" alt="location">
-        </div>
-        <div class="col-5 lg">
-          Mulawin St. Barangay Bitas,<br> Cabanatuan City, Nueva Ecija
-        </div>
-      </div> -->
-      <div class="row iconsto">
-        <img src="icons/geo.svg" alt="location">
-        Mulawin St. Barangay Bitas,<br> Cabanatuan City, Nueva Ecija
-      </div>
-      <!-- <div class="row">
-        <div class="col-sm-3">Level 1: .col-sm-3</div>
-        <div class="col-sm-9">
-          <div class="row">
-            <div class="col-8 col-sm-6">Level 2: .col-8 .col-sm-6</div>
-            <div class="col-4 col-sm-6">Level 2: .col-4 .col-sm-6</div>
-          </div>
-        </div>
-      </div> -->
-
-      <div class="row iconsto">
-        <img src="icons/telephone.svg" alt="">
-        Phone: 0955-617-9958
-      </div>
-      <div class="row iconsto">
-        <img src="icons/email.svg" alt="">
-        Email: dronclinic@gmail.com
-      </div>
-      <div class="row iconsto">
-        <img src="icons/clock.svg" alt="">
-        Opening Hours: 8:00AM<br> Closing Hours: 6:00PM
-      </div>
-    </div>
-  </div>
-</footer>
-
-
-
-</body>
-
 
 </html>
--- send.php
+++ send.php
@@ -9,28 +9,38 @@ require 'PHPMailer-master/src/SMTP.php';
 
 if (isset($_POST["send"])) {
     $mail = new PHPMailer(true);
+    // Enable SMTP debugging (set to 2 for detailed output)
+    $mail->SMTPDebug = 2;
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'ezyvet.neust@gmail.com'; // ur gmail
-    $mail->Password = 'qafo swjl yazl jqxq'; //app pass
-
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port = 465;
+    $mail->Username = 'ezyvet.neust@gmail.com'; // your gmail
+    $mail->Password = 'gjyk hyze xust szfv'; // app password
+    $mail->SMTPSecure = 'ssl'; // Use TLS
+    $mail->Port = 465; // Use port 587 instead of 465
     $mail->setFrom('ezyvet.neust@gmail.com');
     $mail->addAddress($_POST["email"]);
     $mail->isHTML(true);
     $mail->Subject = $_POST['subject'];
     $mail->Body = $_POST['message'];
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
 
-    if ($mail->send()) {
-        echo "
-        <script>
-        alert('sent successfully');
-        document.location.href ='';
-        </script>
-        ";
-    } else {
+    try {
+        if ($mail->send()) {
+            echo "
+            <script>
+            alert('sent successfully');
+            document.location.href ='index.php';
+            </script>
+            ";
+        }
+    } catch (Exception $e) {
         echo "Error sending email: " . $mail->ErrorInfo;
     }
 }
